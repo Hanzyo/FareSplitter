1cf4ff277c4fa0dcd2ded78fcd358d7c4c7c8a83
YouYou Yu <youyy1225@gmail.com>
2023-09-10 13:28:47 -0500

    added visual outputs

--- src/GroceryParser.py
+++ src/GroceryParser.py
@@ -1,6 +1,5 @@
 
 from Groceries import *
-from People import *
 import numpy as np
 class Parser:
     def __init__(self) -> None:
@@ -107,8 +106,20 @@ class Parser:
             for i in range(self.num_people):
                 for j in range(self.num_people):
                     if transactions[i][j] != 0:
-                        file.write(f"{self.index[i]} needs to pay {self.index[j]}: ${transactions[i][j]}\n")
-        
+                        file.write(f"{self.index[i]} needs to pay {self.index[j]}: ${round(transactions[i][j], 2)}\n")
+
+    def print_result(self):
+        with open("receipt.txt", 'w') as file:
+            print("============== FareSplitter Interactive Tool ==============\n")
+            for i in range(self.num_orders):
+                file.write(f"For order #{i+1}: \n")
+                items = self.orders[i]
+                for item in items:
+                    file.write(f"{item.name} -- ${item.price} -- tax rate: {item.tax_rate}\n")
+                    file.write(f"   Splitted by: \n")
+                    for splitter, val in item.splitters.items():
+                        file.write(f"   {splitter} shares ${round(val,2)}\n")
+
     def parse(self):
         self.init_process()
         if(not self.exit_flag):
@@ -119,4 +130,5 @@ class Parser:
         self.calculate()
         if(not self.exit_flag):
             return
+        self.print_result()
         print("Calculation completed, see outputs in output.txt")
